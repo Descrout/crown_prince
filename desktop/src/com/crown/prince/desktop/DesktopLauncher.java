e24fea8c97addc65c9fd25faa51b57e4a46057b7
descrout <descrout@gmail.com>
2019-08-09 00:29:37 +0300

    Initial commit

---
+++ desktop/src/com/crown/prince/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.crown.prince.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.crown.prince.Main;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Main(), config);
+	}
+}
